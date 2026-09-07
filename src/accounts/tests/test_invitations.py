63ad4af0843c03979fe994a4ca31e0eda5350672
masterjay <59769164+beaudelaire1@users.noreply.github.com>
2026-09-06 22:41:06 -0300

    chore: clean invitation tests

--- src/accounts/tests/test_invitations.py
+++ src/accounts/tests/test_invitations.py
@@ -2,7 +2,6 @@ from datetime import timedelta
 from unittest.mock import patch
 
 from django.contrib.auth import get_user_model
-from django.core import mail
 from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
